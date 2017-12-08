8c3e7b2d84be3bc9a22a51b6c1e5b325b853829e
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2017-12-08 16:49:16 -0200

    First commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = 
+    {
+        apiKey: "AIzaSyAFbaUv8uUSH-6Ut2VoPA0Tq68maIZfeLA",
+        authDomain: "shoppinglist-5b345.firebaseapp.com",
+        databaseURL: "https://shoppinglist-5b345.firebaseio.com",
+        projectId: "shoppinglist-5b345",
+        storageBucket: "shoppinglist-5b345.appspot.com",
+        messagingSenderId: "238341112663"
+      };
